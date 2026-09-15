41e0d8f407932b77cbe7e0edeae2b068f171bd98
Prestis <oresthsperrakhs@gmail.com>
2026-09-15 22:50:00 +0300

    Domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-api-domain.com'  // TODO: Replace with your actual production API URL before deploying
+  apiUrl: 'https://lyricmap.gr'
 };
+
